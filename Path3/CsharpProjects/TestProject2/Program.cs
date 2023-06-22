f098b7f03c785d41fb13a3cf41b6c53f62f3ff4d
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-06-22 18:38:46 +0700

    Path 3: Branch the flow of code using the switch-case construct in C#

---
+++ Path3/CsharpProjects/TestProject2/Program.cs
@@ -0,0 +1,27 @@
+﻿bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    value = 10;
+    System.Console.WriteLine($"Inside of code block: {value}");
+}
+
+Console.WriteLine($"Outside of code block: {value}");
+
+
+
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+
+foreach (int number in numbers)
+{
+    bool found = false;
+    total += number;
+
+    if (number == 42) found = true;
+    if (found) Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
