6d02a095853cae328b3b307bbd129f7187f30b73
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-06-24 16:54:14 +0700

    Path 3: Iterate through a code block using for statement in C#

---
+++ Path3/CsharpProjects/TestProject4/Program.cs
@@ -0,0 +1,42 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+foreach (var name in names)
+{
+    // Can't do this:
+    // if (name == "David") name = "Sammy";
+}
+
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+
+for (int i = 0; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0) System.Console.WriteLine("FizzBuzz");
+    else if (i % 3 == 0) System.Console.WriteLine("Fizz");
+    else if (i % 5 == 0) System.Console.WriteLine("Buzz");
+    else System.Console.WriteLine(i);
+}
+
+
